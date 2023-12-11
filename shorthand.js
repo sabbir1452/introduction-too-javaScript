ada2a5ff27465b50e902966610cee6a4351236d2
Admin <sabbir1452>
2023-12-11 19:24:10 +0600

    first commit

---
+++ shorthand.js
@@ -0,0 +1,41 @@
+// variable declaration
+var tomatoPrice = 25;
+// variable value update
+tomatoPrice = 30;
+
+// add something to the previous value
+var price1 = 31;
+var price2 = 10;
+
+// set a new value
+// price1 = 35
+
+// add some value to the existing value
+price1 = price1 + 10;
+// console.log(price1);
+
+// addition to the same variable shorthand
+// += means adding some value to the existing value
+price1 += 10;
+
+// -= means duducting some value from the existing value
+price1 = price1 - 5;
+price2 -= 5; // shorthand -=
+console.log(price2);
+
+// *=
+price1 *= 5;
+
+// /=
+price1 /= 5;
+console.log(price1);
+
+var age = 15;
+age = age + 1;
+// ++ means adding 1 to teh existing
+age++;
+
+var love = 100;
+love = love - 1;
+// -- means you are duducting 1 from the existing value
+love--;
---
+++ variable-name.js
@@ -0,0 +1,31 @@
+// good variable
+var price = 29;
+
+// 1. variable name can not be any keywords
+var false = 95;
+var return = true;
+
+// 2. variable name has to be in one work. NO space
+var my home addres ="New California";
+
+
+// 3. Variable name can not have quotation
+var "name" ="Toma Hanks";
+
+// 4. variable name can not starts with a number but can ends with a number
+var 99Club = 1964;
+var club25 = 2025;
+
+// 5. How to use long names
+// can not use dash
+var user-name = "raj bappa"
+
+var user_name= "bappa raj";
+var usercurrenthomeaddress = "andor killa bandor ban"
+var user_home_address = "andor killa bandor ban"// Snake case
+var userHomeAddress = "andor killa bandor ban" // Camel case : we will use this one
+var UseHomeAddress = "andor killa bandor ban" // pascal case
+
+// variable name is case sensitive
+var person = 25;
+var Person = 35;
